753d596229fe2aa733570d4f7e2ac4f7896b384a
L.Nelli <52744702+B4dWeather@users.noreply.github.com>
2024-07-17 17:42:48 +0200

    compressed reading
    STATUS: not working (error on chunk type when not yielding it)

--- src/config.py
+++ src/config.py
@@ -2,10 +2,13 @@
 IMPORTANT: THIS FILE IS NOT UPDATED BY GIT
 IT REQUIRES MANUAL CONFIGURATION ON EACH MACHINE
 """
+''' LOCAL PATHS '''
+# path lorenzo
+full_collection_compressed = "D:/Repositories/mircv/dataset/collection.tar.gz"
+test_collection_uncompressed = "D:/Repositories/mircv/dataset/test.tsv"
 
 '''collection_path : this is the LOCAL path to the dataset.'''
-collection_path_config = "D:/Repositories/mircv/dataset/collection.tsv"  # lorenzo
-
+collection_path_config = full_collection_compressed
 '''
 Dataset Specification. You must use the MSMARCO Passages collection1 available on
 this page: https://microsoft.github.io/msmarco/TREC-Deep-Learning-2020. Scroll
@@ -36,3 +39,38 @@ Your program must include an evaluation of the systems using a standard
 collection such as the TREC DL 2019 queries and TREC DL 2019 qrels, or the
 TREC DL 2020 queries and TREC DL 2020 qrels.
 '''
+
+"""Debug Verbosity"""
+''' Raising the verbosity level modify the verbosity of the python console during the execution'''
+verbosity_config = 5
+
+
+# 0 : only errors
+# 1 : processes and modules
+# 2 : critical blocks
+# 3 : function calls
+# 4 : row by row (debug)
+# 5 : loop iterations
+def print_log(msg, priority=0):
+    global verbosity_config
+    if priority >= verbosity_config:
+        print(msg)
+
+
+'''
+TEST MODE WITH LIMITED ROWS
+Pick a maximum number of lines to be read from dataset. 
+If limit is zero, then all the rows are going to  be read (no limit)
+'''
+# execution time: (compressed file)
+# 1000 rows -> 12 s
+# 3800 rows -> 18 s
+# 100000 rows -> 47 s
+limit_input_rows_config = 100000
+
+'''
+CHUNK SIZE CONFIG
+split the input file in smaller chunks, to avoid processing it at once.
+chunk size is the number of rows in each chunk
+'''
+chunk_size_config = 5
--- src/tests/t1_verify_dataset.py
+++ src/tests/t1_verify_dataset.py
@@ -1,11 +1,11 @@
-from src.modules.documentProcessing import fetch_data_row_from_collection
+from src.modules.documentProcessing import open_dataset
 
-data = fetch_data_row_from_collection(4)
+data = open_dataset()
 
 # data is a dataframe. it contains two columns: index and value.
 # each value is like this : id \t text
 # index and id are supposed to match, for each row
 
 print(data)
-print(data.dtype)
-print(len(data))
+#print(data.dtype)
+#print(len(data))
