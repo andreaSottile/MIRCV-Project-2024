babd3654e336dac9bc100f417d59016912e68804
L.Nelli <52744702+B4dWeather@users.noreply.github.com>
2024-07-21 16:56:32 +0200

    document parsing completed
    test 1 correctly working on:
    - reading collection
    - reading n lines
    - reading 1 line
    - both compressed and uncompressed file

--- src/modules/documentProcessing.py
+++ src/modules/documentProcessing.py
@@ -18,16 +18,9 @@ stemming algorithm or similar. For stopword removal, you can use any english
 stopwords list available on the Web. Ideally, your program should have a compile
 flag that allows you to enalbe/disble stemming & stopword removal.
 """
-import csv
-
 import pandas as pd
-from pandas import DataFrame
-import csv
-import pandas as pd
-from pandas import DataFrame
-import tarfile
 import io
-
+import csv
 from src.config import collection_path_config, print_log, limit_input_rows_config, chunk_size_config
 import tarfile
 
@@ -36,8 +29,6 @@ read_rows = 0
 
 def open_dataset():
     global read_rows
-    # avoid pandas truncation
-    pd.set_option('display.max_colwidth', None)
     # reset row counter
     read_rows = 0
     print_log("opening dataset file", priority=3)
@@ -50,106 +41,46 @@ def open_dataset():
             print_log("tar.gz uncompression: starting", priority=4)
             dataset_raw = tar.extractfile(member)
             print_log("tar.gz uncompression: finished", priority=4)
+            dataset_wrapped = io.TextIOWrapper(dataset_raw, encoding='utf-8')
 
-            for chunk in split_tsv_chunks(dataset_raw, chunk_size_config):
-                print_log("read progress: " + str(read_rows), priority=5)
-                if 0 < limit_input_rows_config <= read_rows:
+            while True:
+                line = dataset_wrapped.readline()
+                if line:
+                    print_log("read progress: " + str(read_rows), priority=5)
+                    if 0 < limit_input_rows_config <= read_rows:
+                        break
+                    content = line.strip().split("\t")
+                    if len(content) == 2:
+                        process_dataset_row(content[0], content[1])
+                    else:
+                        print_log("invalid line len at row " + str(read_rows), priority=4)
+
+                    read_rows += 1
+                else:
                     break
-                process_dataset_chunk(chunk)
-                #read_rows += chunk_size_config
-
             print_log("read finished", priority=4)
 
-    if collection_path_config.endswith(".tsv"):
+    elif collection_path_config.endswith(".tsv"):
         # working with .tsv file
         print_log("Opening .tsv file", priority=2)
 
-        for chunk in split_tsv_chunks(collection_path_config, chunk_size_config):
-            print_log("read progress: " + str(read_rows), priority=5)
+        dataset = pd.read_csv(collection_path_config, sep='\t', header=None)
+        for line in dataset.values:
             if 0 < limit_input_rows_config <= read_rows:
                 break
-            process_dataset_chunk(chunk)
-            #read_rows += chunk_size_config
+            print(line)
+            if len(line) == 2:
+                process_dataset_row(line[0], line[1])
+            else:
+                print_log("invalid line len at row " + str(read_rows), priority=4)
 
+            read_rows += 1
         print_log("read finished", priority=4)
+
+
 
     print_log("input phase done, returning to parsing", priority=3)
     return "rows read from dataset: " + str(read_rows)
-
-
-def split_tsv_chunks(file, chunk_size=1, is_stream=True):
-    global read_rows
-    excluded = []
-    buffer = []
-
-    if is_stream:
-        file = io.TextIOWrapper(file, encoding='utf-8')
-
-    while True:
-        try:
-            line = file.readline()
-            if not line:
-                # Se il buffer contiene ancora righe alla fine del file, restituisci l'ultimo chunk
-                if buffer:
-                    chunk = pd.DataFrame(buffer)
-                    yield chunk
-                break
-
-            buffer.append(line.strip().split('\t'))
-            if len(buffer) >= chunk_size:
-                chunk = pd.DataFrame(buffer)
-                yield chunk
-                buffer = []  # Svuota il buffer per il prossimo chunk
-
-        except Exception as e:
-            print_log(f"error on parsing near row {read_rows}: {str(e)}", priority=3)
-            excluded.append(read_rows)
-            continue
-
-        read_rows += 1  # Incrementa il contatore delle righe lette
-
-    print(len(excluded))
-    print(excluded)
-
-
-def split_tsv_chunks2(file_path, chunk_size=1):
-    global read_rows
-    excluded = []
-    while True:
-        try:
-            chunk = pd.read_csv(file_path, sep='\t', header=None, skiprows=read_rows,
-                                nrows=chunk_size, encoding='utf-8', engine='python')
-            if chunk.empty:
-                break
-            yield chunk
-        except pd.errors.EmptyDataError:
-            # Break the loop if there are no more data to read
-            break
-        except pd.errors.ParserError:
-            print_log("error on parsing near row " + str(read_rows), priority=3)
-            excluded.append(read_rows)
-            continue
-        except UnicodeDecodeError:
-            print_log("unreadable char found near row " + str(read_rows), priority=3)
-            excluded.append(read_rows)
-            continue
-    print(len(excluded))
-    print(excluded)
-
-
-def process_dataset_chunk(chunk):
-    global read_rows
-    if type(chunk) is DataFrame:
-        # check that each row has two elements <id,text>
-        if chunk.values.shape[1] == 2:
-            for doc_id, doc_text in chunk.values:
-                process_dataset_row(doc_id, doc_text)
-        else:
-            print_log("input error, invalid chunk \n == dumping invalid row ==", priority=3)
-            print_log(str(chunk), priority=3)
-            print_log("== end of dump ==", priority=3)
-    else:
-        print_log("invalid chunk format near line " + str(read_rows), priority=3)
 
 
 def process_dataset_row(d_id, d_text):
@@ -166,13 +97,56 @@ def process_dataset_row(d_id, d_text):
 
 def fetch_data_row_from_collection(row_index):
     # @param row_index : the id of the document in the collection
-    return fetch_n_data_rows_from_collection(row_index, 1 + row_index)[0]
+    return fetch_n_data_rows_from_collection(row_index, 1 + row_index)
 
 
 def fetch_n_data_rows_from_collection(start_row, stop_row):
     # @param start_row : start reading the collection from here (avoid reading all the collection)
     # @param end_row : stop reading after n rows (avoid reading all the collection)
     n_rows = stop_row - start_row
-    print("fetching " + str(n_rows) + " from collection")
-    res_dataframe = pd.read_csv(collection_path_config, sep='\t', header=None, skiprows=start_row, nrows=n_rows)
-    return res_dataframe.values
+    result_id = []
+    result_txt = []
+    target_row = []
+    for n in range(n_rows):
+        target_row.append(n + start_row)
+    print_log("fetching " + str(n_rows) + " from collection", priority=3)
+    if collection_path_config.endswith(".gz"):
+        # working with compressed file, required uncompression
+        print_log("Opening tar.gz file", priority=2)
+
+        tar = tarfile.open(collection_path_config, "r:gz")
+        for member in tar.getmembers():
+            print_log("tar.gz uncompression: starting", priority=4)
+            dataset_raw = tar.extractfile(member)
+            print_log("tar.gz uncompression: finished", priority=4)
+
+            dataset = pd.read_csv(dataset_raw, sep='\t', header=None, skiprows=start_row, nrows=n_rows,
+                                  encoding="utf-8", engine="python")
+
+            for target in target_row:
+                raw_result = dataset.loc[dataset[0] == target].values
+                raw_result = raw_result[0]
+                print(raw_result)
+                if len(raw_result) == 2:
+                    print_log("reading line " + str(target), priority=5)
+                    result_id.append(raw_result[0])
+                    result_txt.append(raw_result[1])
+                else:
+                    print_log("cannot find line " + str(target), priority=4)
+
+    elif collection_path_config.endswith(".tsv"):
+        dataset = pd.read_csv(collection_path_config, sep='\t', header=None, skiprows=start_row, nrows=n_rows,
+                              encoding="utf-8", engine="python")
+
+        for target in target_row:
+            raw_result = dataset.loc[dataset[0] == target].values
+            raw_result = raw_result[0]
+            print(raw_result)
+            if len(raw_result) == 2:
+                print_log("reading line " + str(target), priority=5)
+                result_id.append(raw_result[0])
+                result_txt.append(raw_result[1])
+            else:
+                print_log("cannot find line " + str(target), priority=4)
+    print_log("read finished", priority=4)
+    return result_id, result_txt
--- src/tests/t1_verify_dataset.py
+++ src/tests/t1_verify_dataset.py
@@ -1,5 +1,8 @@
-from src.modules.documentProcessing import open_dataset
+import time
 
+from src.modules.documentProcessing import open_dataset, fetch_data_row_from_collection
+
+print("reading some files")
 data = open_dataset()
 
 # data is a dataframe. it contains two columns: index and value.
@@ -7,5 +10,23 @@ data = open_dataset()
 # index and id are supposed to match, for each row
 
 print(data)
-#print(data.dtype)
-#print(len(data))
+
+
+def test_read(docid):
+    tic = time.perf_counter()
+    d, t = fetch_data_row_from_collection(docid)
+    print(d)
+    print(t[0])
+    toc = time.perf_counter()
+    print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
+
+
+# docid    # query on compressed   # query on uncompressed
+test_read(16)  # 10.57 s                0.0036 s
+test_read(726)  # 10.83 s                0.0030 s
+test_read(1032456)  # 13.18 s                2.6074 s
+test_read(7833456)  # 29.48 s                19.9869 s
+test_read(65562)  # 11,24 s                0.1576 s
+test_read(684564)  # 12,39 s                1.7179 s
+# the time is different thanks to panda's skiprows+nrows.
+# without using skiprows+nrows, each teat_read takes 49-52 sec
