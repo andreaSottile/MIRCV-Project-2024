16cf02d4f6f4d367694374175c4bfe0de2bfeed1
L.Nelli <52744702+B4dWeather@users.noreply.github.com>
2024-07-23 17:26:51 +0200

    new files
    added files with compression and searchresult
    they're just named and filled with TODOs

---
+++ src/modules/compression.py
@@ -0,0 +1,6 @@
+from src.config import print_log
+
+
+def compress_index(index_name, index_file_path, index_lexicon_path):
+    # compression of index file
+    print_log("compressing index file", priority=1)
---
+++ src/modules/searchResult.py
@@ -0,0 +1,7 @@
+class searchResult:
+    def __init__(self, k):
+        self.list = []
+        self.k = k
+
+    def append_result(self, item, score):
+        pass
